2f96bbf09c0e8340ef9df5ce6c9d7b64ccf01603
north-west-wu <83222527+north-west-wu@users.noreply.github.com>
2022-08-26 16:03:46 +0800

    Add files via upload

---
+++ MyMath/Bound2.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using UnityEngine;
+
+namespace MyMath
+{
+    public struct Bound2
+    {
+        #region 字段
+
+        public Vec2 min;
+        public Vec2 max;
+
+        #endregion
+        
+        #region 属性
+
+        public Vec3 this[int index]
+        {
+            get
+            {
+                if (index < 0 || index > 7)
+                    throw new IndexOutOfRangeException();
+
+                return new Vec2(
+                    (index & 1) == 0 ? min.x : max.x,
+                    (index & 2) == 0 ? min.y : max.y);
+            }
+        }
+
+        public bool IsEmpty => min.x > max.x || min.y > max.y;
+        
+        public Vec2 Center => (max + min) * 0.5f;
+        
+        public Vec2 Size => max - min;
+
+        public Vec2 Extents => (max - min) * 0.5f;
+
+        public float Width => max.x - min.x;
+
+        public float Height => max.y - min.y;
+
+        public Rect ToRect => new Rect(min.x, max.y, Height, Width);
+
+        #endregion
+
+        #region 方法
+        
+        public Vec2 ClosestPoint(Vec2 p)
+        {
+            if (p.x < min.x) p.x = min.x;
+            else if (p.x > max.x) p.x = max.x;
+
+            if (p.y < min.y) p.y = min.y;
+            else if (p.y > max.y) p.y = max.y;
+
+            return p;
+        }
+
+        public bool Contains(Vec2 point)
+        {
+            return point.x >= min.x && point.x <= max.x &&
+                   point.y >= min.y && point.y <= max.y;
+        }
+
+        public void Clear()
+        {
+            min.x = min.y = float.MaxValue;
+            max.x = max.y = float.MinValue;
+        }
+
+        public void Encapsulate(Vec2 point)
+        {
+            if (point.x < min.x) min.x = point.x;
+            if (point.x > max.x) max.x = point.x;
+
+            if (point.y < min.y) min.y = point.y;
+            if (point.y > max.y) max.y = point.y;
+        }
+
+        public void Encapsulate(Bound2 box)
+        {
+            if (box.min.x < min.x) min.x = box.min.x;
+            if (box.max.x > max.x) max.x = box.max.x;
+
+            if (box.min.y < min.y) min.y = box.min.y;
+            if (box.max.y > max.y) max.y = box.max.y;
+        }
+
+        #endregion
+    }
+}
---
+++ MyMath/Plane.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Plane = MyMath.Plane;
+
+namespace MyMath
+{
+    public struct Plane
+    {
+        #region 字段
+
+        public Vec3 normal;
+        public float distance;
+
+        #endregion
+
+        #region 属性
+
+        public Plane Flipped => new Plane(-normal, -distance);
+
+        #endregion
+
+        #region 构造函数
+
+        public Plane(Vec3 normal, float distance)
+        {
+            this.normal = normal;
+            this.distance = distance;
+        }
+
+        //三点取面的写法
+        public Plane(Vec3 p1, Vec3 p2, Vec3 p3)
+        {
+            Vec3 v1 = p2 - p1;
+            Vec3 v2 = p3 - p2;
+
+            normal = Vec3.Cross(v1, v2).Normalized;
+            distance = Vec3.Dot(p1, normal);
+        }
+
+        //对于多边形的最佳拟合平面
+        public Plane(params Vec3[] arr)
+        {
+            int n = arr.Length;
+            if (n < 3) throw new AggregateException("矢量数量不足3");
+
+            Vec3 v = Vec3.Zero;
+
+            Vec3 p1 = arr[n - 1];
+
+            for (int i = 0; i < n; i++)
+            {
+                Vec3 p2 = arr[i];
+
+                v.x += (p2.z + p1.z) * (p2.y - p1.y);
+                v.y += (p2.x + p1.x) * (p2.z - p1.z);
+                v.z += (p2.y + p1.y) * (p2.x - p1.x);
+
+                p1 = p2;
+            }
+
+            normal = v.Normalized;
+            distance = Vec3.Dot(arr[0], normal);
+        }
+
+        #endregion
+
+        #region 方法
+        
+        public Vec3 ClosestPoint(Vec3 p)
+        {
+            float a = DistanceToPoint(p);
+            return p - a * normal;
+        }
+
+        public float DistanceToPoint(Vec3 p)
+        {
+            return Vec3.Dot(p, normal) - distance;
+        }
+
+        public bool Side(Vec3 p)
+        {
+            return DistanceToPoint(p) > 0f;
+        }
+
+        public bool SameSide(Vec3 p1, Vec3 p2)
+        {
+            float n1 = DistanceToPoint(p1);
+            float n2 = DistanceToPoint(p2);
+
+            return (n1 >= 0f && n2 >= 0f) || (n1 < 0f && n2 < 0f);
+        }
+
+        #endregion
+    }
+}
